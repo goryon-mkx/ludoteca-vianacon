8e9b128ff76acd2651fe08302efa7386816d842f
Fábio Ferreira <f.ferreira@kigroup.de>
2020-12-08 15:43:55 +0000

    Added static.json

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
- const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+ // const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
-     publicPath: IS_PRODUCTION
-     ? 'https://d3i9hwjfzmc2xm.cloudfront.net'
-     : '/',
+     // publicPath: IS_PRODUCTION
+     // ? 'https://d3i9hwjfzmc2xm.cloudfront.net'
+     // : '/',
     // For Production, replace set baseUrl to CDN
     // And set the CDN origin to `yourdomain.com/static`
     // Whitenoise will serve once to CDN which will then cache
