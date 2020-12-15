62531bbbb20cd150fb84609e6a82656930de7171
Fábio Ferreira <f.ferreira@kigroup.de>
2020-12-15 08:29:46 +0000

    Added route for whitenoise history mode support

--- backend/urls.py
+++ backend/urls.py
@@ -10,7 +10,7 @@ from django.views.generic import TemplateView
 from .api.views import index_view
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-
+from django.http import FileResponse
 from backend.api import views
 
 router = routers.DefaultRouter()
@@ -36,5 +36,9 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
+    path('<path:path>', index_view),
 ]
+
+
+def index_view(request, path):
+    return FileResponse(open('/public/index.html', 'rb'))
