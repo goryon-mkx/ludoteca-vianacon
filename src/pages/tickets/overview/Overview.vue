cb4abd1b864b8b930a0b6ef02f4b8060b339a508
Fábio Ferreira <fabiogasparferreira@gmail.com>
2022-11-22 22:23:15 +0000

    Ticket small fixes

--- src/pages/tickets/overview/Overview.vue
+++ src/pages/tickets/overview/Overview.vue
@@ -93,7 +93,7 @@
               Login
             </b-button>
             <b-alert v-else-if="!isAssociate() && !isStaff()" show variant="light"><b-icon-award-fill/> Member exclusive. See how to become one <b-link target="_blank" href="https://www.spielportugal.org/membership">here</b-link></b-alert>
-            <b-alert v-else-if="this.hasPreviousOrders" show variant="warning"><b-icon-exclamation-circle-fill/> One purchase per Member</b-alert>
+            <b-alert v-else-if="this.hasPreviousOrders && !isStaff()" show variant="warning"><b-icon-exclamation-circle-fill/> One purchase per Member</b-alert>
             <b-button v-else :to="{name: 'BuyTickets'}" variant="primary" block>
               Buy
             </b-button>
